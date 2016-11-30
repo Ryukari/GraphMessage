d59f8165bef26f2bf00262389ef6f9e9f4f86e2a
Andres E. Rojas M <anderojasm@hotmail.com>
2016-11-29 18:32:12 -0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,20 @@
+package com.aerojasm.graphmessage;
+
+/**
+ * Created by aerojasm on 28/11/16.
+ */
+
+public class User {
+
+    private String name;
+    private String lastName;
+    private String MACaddres;
+
+    public User(String pname, String plastName, String pMACaddress){
+        name = pname;
+        lastName = plastName;
+        MACaddres = pMACaddress;
+
+    }
+
+}
